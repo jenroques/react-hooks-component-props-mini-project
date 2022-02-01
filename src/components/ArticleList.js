f9c667a719be32596458184ec9af3b14b031581e
Jennifer <jenniferroques@gmail.com>
2022-02-01 13:25:40 -0600

    Completed

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import Article from "./Article";
+
+
+function ArticleList({ posts }) {
+    console.log(posts)
+    const articleArray = posts.map((post) => {
+        return (
+        <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+        );
+    });
+    return (
+        <main>{articleArray}</main>
+    );
+}
+
+export default ArticleList;
